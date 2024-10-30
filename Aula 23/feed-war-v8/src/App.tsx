8cff7fcf0800d6be20886e84f812593f45a05b29
Rafael-Patricio <rafaelpatricio1@outlook.com>
2024-10-30 20:12:41 -0300

    inicio dos componentes

--- Aula 23/feed-war-v8/src/App.tsx
+++ Aula 23/feed-war-v8/src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Title } from './components/Title'
 
 export function App() {
-  const [count, setCount] = useState(0)
   const text = 'Hello World!'
   const description = 'Its my first page'
+
   return (
     <>
-    <h1>{text}</h1>
+    <Title />
     <p>{description}</p>
     </>
   )
---
+++ Aula 23/feed-war-v8/src/components/Title.tsx
@@ -0,0 +1,10 @@
+// kebab case => nome-dividido-por-traco
+// snake case => nome_dividido_por_underline
+// camel case => divididoPorMaiuscula (a partir da segunda palavra)
+// pascal case => DivididoPorMaiuscula (a partir da primeira palavra)
+
+export function Title() {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
