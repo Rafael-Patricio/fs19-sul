ccd25c36d97e684ad0fd534eff44059f28058a86
Rafael-Patricio <rafaelpatricio1@outlook.com>
2024-10-30 22:03:26 -0300

    configuração do shadcn

--- Aula 23/feed-war-v8/src/App.tsx
+++ Aula 23/feed-war-v8/src/App.tsx
@@ -1,5 +1,6 @@
 import { Title } from './components/Title'
 import { Description } from './components/Description'
+import { Button } from './components/ui/button'
 
 export function App() {
 
@@ -7,6 +8,9 @@ export function App() {
     <>
     <Title value='Hello World!' />
     <Description value='Its my first page!' />
+    <Button variant={'outline'}>Default</Button>
+    <Button variant={'secondary'}>Secondary</Button>
+    <Button variant={'destructive'}>Destructive</Button>
     </>
   )
 }
